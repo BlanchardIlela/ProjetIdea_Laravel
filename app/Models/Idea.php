1803db825c0388fc958948c9aaa48a2cd26554ac
BlanchardIlela <blancilela@gmail.com>
2026-08-13 00:03:12 +0100

    Les donnnées dans mon models

---
+++ app/Models/Idea.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\IdeaStatus;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Idea extends Model
+{
+    /** @use HasFactory<\Database\Factories\IdeaFactory> */
+    use HasFactory;
+
+    //Définition des règles de conversion automatique d'attributs ($casts).
+    protected $casts = [
+        /**
+         * Convertit la colone links de la BDD qui est en format JSON
+         * en un objet mannipulable comme un tableau PHP.
+         */
+        'links' => AsArrayObject::class,
+        /**
+         * Convertit la valeur texte de la BDD directement
+         * en une instance de notre enum IdeaStatus.
+         */
+        'status' => IdeaStatus::class
+    ];
+
+    public function user(): BelongsTo
+    {
+        //Établit une relation Many-to-One (via la clé étrangère).
+        return $this->belongsTo(User::class);
+    }
+}
