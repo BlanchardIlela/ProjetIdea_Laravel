ec086ce055f9894cb8ebf8b0a202eec070c2a0dc
BlanchardIlela <blancilela@gmail.com>
2026-08-15 10:32:20 +0100

    La méthode statusConts permet de selectionner le status par groupe

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use App\IdeaStatus;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 
 class Idea extends Model
 {
@@ -33,6 +35,23 @@ class Idea extends Model
     protected $attributes = [
         'status' => IdeaStatus::PENDING->value,
     ];
+
+    public static function statusCounts(User $user):Collection
+    {
+        //Selectionner le status par groupe.
+        $count = Idea::query()
+            ->where('user_id', Auth::id())
+            ->selectRaw('status, count(*) as count')
+            ->groupBy('status')
+            ->pluck('count', 'status');
+
+        return collect(IdeaStatus::cases())
+            ->mapWithKeys(fn ($status) => [
+                $status->value => $count->get($status->value, 0),
+            ])
+            ->put('all', Idea::query()->where('user_id', Auth::id())->count());
+
+    }
 
     public function user(): BelongsTo
     {
